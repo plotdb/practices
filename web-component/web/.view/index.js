a5e64dbce7de4af769ea9e53ce3d46a0371a8c0a
Kirby Wu <tkirby@gmail.com>
2021-01-27 15:31:30 +0800

    init commit

---
+++ web-component/web/.view/index.js
@@ -0,0 +1,14 @@
+ (function() { function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;pug_debug_line = 1;pug_debug_filename = "src\u002Fpug\u002Findex.pug";
+pug_html = pug_html + "\u003C!DOCTYPE html\u003E";
+;pug_debug_line = 2;pug_debug_filename = "src\u002Fpug\u002Findex.pug";
+pug_html = pug_html + "\u003Chtml\u003E";
+;pug_debug_line = 3;pug_debug_filename = "src\u002Fpug\u002Findex.pug";
+pug_html = pug_html + "\u003Chead\u003E\u003C\u002Fhead\u003E";
+;pug_debug_line = 4;pug_debug_filename = "src\u002Fpug\u002Findex.pug";
+pug_html = pug_html + "\u003Cbody\u003E";
+;pug_debug_line = 5;pug_debug_filename = "src\u002Fpug\u002Findex.pug";
+pug_html = pug_html + "\u003Cc-e\u003E\u003C\u002Fc-e\u003E";
+;pug_debug_line = 6;pug_debug_filename = "src\u002Fpug\u002Findex.pug";
+pug_html = pug_html + "\u003Cscript\u003E";
+;pug_debug_line = 6;pug_debug_filename = "src\u002Fpug\u002Findex.pug";
+pug_html = pug_html + "var zzz, node;\nzzz = function(){\n  var ret;\n  ret = Reflect.construct(HTMLElement, [], zzz);\n  console.log(ret);\n  ret.test = 13;\n  return ret;\n};\nzzz.observedAttributes = ['hi'];\nObject.setPrototypeOf(zzz.prototype, HTMLElement.prototype);\nObject.setPrototypeOf(zzz, HTMLElement);\nimport$(zzz.prototype, {\n  connectedCallback: function(){\n    return console.log('connected');\n  },\n  disconnectedCallback: function(){\n    return console.log('disconnected');\n  },\n  adoptedCallback: function(){\n    return console.log('adopted');\n  },\n  attributeChangedCallback: function(name, o, n){\n    return console.log(\"changed: \", name, o, n);\n  },\n  hello: function(){\n    return console.log('hello');\n  }\n});\ncustomElements.define(\"c-e\", zzz);\nnode = document.querySelector('c-e');\nconsole.log(node.nodeType, node.nodeName, node.test);\nnode.hello();\nnode.setAttribute('hi', 1);\nfunction import$(obj, src){\n  var own = {}.hasOwnProperty;\n  for (var key in src) if (own.call(src, key)) obj[key] = src[key];\n  return obj;\n}\u003C\u002Fscript\u003E\u003C\u002Fbody\u003E\u003C\u002Fhtml\u003E";} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line);};return pug_html;}; module.exports = template; })() 
